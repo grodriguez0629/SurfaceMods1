b060068eb63260c06babc3c4776291e4ee4a6e5f
saruhj <96214729+saruhj@users.noreply.github.com>
2025-02-06 14:23:58 -0500

    upload initial test code

---
+++ initialStepperTest.ino
@@ -0,0 +1,24 @@
+//Map pins for DIR and STEP
+#define direction_pin 5
+#define stepper_pin 6
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(direction_pin, OUTPUT);
+  pinMode(stepper_pin, OUTPUT);
+
+  //Sets the initial direction of the stepper motor (based on where A1, A2, B1, and B2 are connected)
+  digitalWrite(DIR_PIN, HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   
+  //Takes 200 steps in the direction specified.
+  for(int i = 0; i < 200; i++) {
+    digitalWrite(stepper_pin, HIGH);  
+    //Controls the speed at which the steps are taken. Higher delay = Slower, Shorter delay = Faster.
+    delayMicroseconds(50);
+  }
+}
