eca3ee66c9bd94b14dcff0361c6da0c8fcc1e619
saruhj <96214729+saruhj@users.noreply.github.com>
2025-03-20 17:18:21 -0400

    Update ExtremelyBasicDriveCode.ino

--- ExtremelyBasicDriveCode.ino
+++ ExtremelyBasicDriveCode.ino
@@ -1,24 +1,50 @@
-#define stepPin 34
-#define dirPin 36
+/*
+    Controlling multiple steppers with the AccelStepper and MultiStepper library
 
+     by Dejan, https://howtomechatronics.com
+*/
+
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+
+// Define the stepper motor and the pins that is connected to
+AccelStepper stepper1(1, 34, 36); // (Typeof driver: with 2 pins, STEP, DIR)
+AccelStepper stepper2(1, 3, 6);
+AccelStepper stepper3(1, 4, 7);
+
+MultiStepper steppersControl;  // Create instance of MultiStepper
+
+long gotoposition[3]; // An array to store the target positions for each stepper motor
 
 void setup() {
-  // put your setup code here, to run once:
-   Serial.begin(9600);
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+
+  stepper1.setMaxSpeed(1000); // Set maximum speed value for the stepper
+  stepper2.setMaxSpeed(1000);
+  stepper3.setMaxSpeed(1000);
+
+  // Adding the 3 steppers to the steppersControl instance for multi stepper control
+  steppersControl.addStepper(stepper1);
+  steppersControl.addStepper(stepper2);
+  steppersControl.addStepper(stepper3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(dirPin,HIGH);
-  Serial.println("Starting loop...");
-  for(int x = 0; x < 800; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(700);    // by changing this time delay between the steps we can change the rotation speed
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(700); 
-    Serial.println("Looping...");
-  }
-  Serial.println("Loop finished...");
+  // Store the target positions in the "gotopostion" array
+  gotoposition[0] = 800;  // 800 steps - full rotation with quater-step resolution
+  gotoposition[1] = 1600;
+  gotoposition[2] = 3200;
+
+  steppersControl.moveTo(gotoposition); // Calculates the required speed for all motors
+  steppersControl.runSpeedToPosition(); // Blocks until all steppers are in position
+
+  delay(1000);
+
+  gotoposition[0] = 0;
+  gotoposition[1] = 0;
+  gotoposition[2] = 0;
+
+  steppersControl.moveTo(gotoposition);
+  steppersControl.runSpeedToPosition();
+
+  delay(1000);
 }
