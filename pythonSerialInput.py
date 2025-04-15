4bd62d36406e57e964a9b50515ac553e4c34bda7
Jesus De La Rosa <117212034+snohoho@users.noreply.github.com>
2025-04-15 15:38:51 -0400

    Add files via upload

--- pythonSerialInput.py
+++ pythonSerialInput.py
@@ -8,7 +8,7 @@ from tkinter import ttk
 #serial initialization
 #serial port for communication
 #baudrate
-SERIALPORT = 'COM3'
+SERIALPORT = 'COM_3'
 BAUDRATE = 9600
 #board = serial.Serial(port=SERIALPORT, baudrate=BAUDRATE)
 
@@ -17,18 +17,18 @@ def send_movement(steps, dir):
     print("moving rover " + steps.get() + " steps " + dir.get())
     cmd = steps.get() + " " + dir.get()
     print(cmd)
- #   board.write(cmd.encode())
+    #board.write(cmd.encode())
 
 #sends shoulder movement to the board
-def send_shoulder(side, dir):
-    print("moving " + side.get() + " shoulder " + dir.get())
-    cmd = side.get() + " " + dir.get()
+def send_shoulder(deg, side, dir):
+    print("moving " + side.get() + " shoulder " + dir.get() + " " + deg.get() + " degrees")
+    cmd = side.get() + " " + dir.get() + " " + deg.get()
     print(cmd)
-  #  board.write(cmd.encode())
-
+    #board.write(cmd.encode())
+    
 #sends auger movemnt
 def send_auger(spd_prof, dir):
-    print("spd profile " + spd_prof.get() + " at dir " + dir.get())
+    print("starting auger at " + spd_prof.get() + " in the " + dir.get() + " direction")
     cmd = spd_prof.get() + " " + dir.get()
     print(cmd)
    # board.write(cmd.encode())
@@ -50,6 +50,12 @@ def enable_btn(btn, arg1, arg2):
     elif(arg1.get() == '' or arg2.get() == ''):
         btn.state(['disabled'])
 
+def enable_btn2(btn, arg1, arg2, arg3):
+    if(arg1.get() != '' and arg2.get() != '' and arg3.get() != ''):
+        btn.state(['!disabled'])
+    elif(arg1.get() == '' or arg2.get() == '' or arg3.get() == ''):
+        btn.state(['disabled'])
+
 def main():
     root = Tk()
     root.title("RASSOR movement interface")
@@ -68,6 +74,8 @@ def main():
     move_btn = ttk.Button(frame, text="MOVE ROVER",
                           command=lambda *args: send_movement(step_entry, move_flrb), state='disabled')
     move_btn.grid(column=0, row=1)
+    
+    ttk.Label(frame, text="Steps to Move").grid(column=1, row=1)
     ttk.Entry(frame, 
               width=5, 
               textvariable=step_entry).grid(column=2, row=1)
@@ -89,49 +97,56 @@ def main():
                     command=lambda *args: enable_btn(move_btn, step_entry, move_flrb)).grid(column=2, row=4)
 
     #shoulder movement handling UI
+    deg_entry = StringVar()
+    deg_entry.trace_add("write", lambda *args: limit_char_count(step_entry, 2))
+    deg_entry.trace_add("write", lambda *args: enable_btn2(shoulder_btn, deg_entry, shoulder_lr, shoulder_ud))
     shoulder_lr = StringVar()
     shoulder_ud = StringVar()
     
     shoulder_btn = ttk.Button(frame, text="MOVE SHOULDER", 
-                              command=lambda *args: send_shoulder(shoulder_lr, shoulder_ud), state='disabled')
+                              command=lambda *args: send_shoulder(deg_entry, shoulder_lr, shoulder_ud), state='disabled')
     shoulder_btn.grid(column=0, row=5)
-    ttk.Label(frame, text="Shoulder to Rotate").grid(column=1, row=5)
+    ttk.Label(frame, text="Degrees to Rotate").grid(column=1, row=5)
+    ttk.Entry(frame, 
+              width=5, 
+              textvariable=deg_entry).grid(column=2, row=5)
+    ttk.Label(frame, text="Shoulder to Rotate").grid(column=1, row=6)
     ttk.Radiobutton(frame, text="Left", 
                     variable=shoulder_lr, 
                     value='L', 
-                    command=lambda *args: enable_btn(shoulder_btn, shoulder_lr, shoulder_ud)).grid(column=1, row=6)
+                    command=lambda *args: enable_btn2(shoulder_btn, deg_entry, shoulder_lr, shoulder_ud)).grid(column=1, row=7)
     ttk.Radiobutton(frame, text="Right", 
                     variable=shoulder_lr, 
                     value='R', 
-                    command=lambda *args: enable_btn(shoulder_btn, shoulder_lr, shoulder_ud)).grid(column=2, row=6)
-    ttk.Label(frame, text="Direction to Rotate").grid(column=1, row=7)
+                    command=lambda *args: enable_btn2(shoulder_btn, deg_entry, shoulder_lr, shoulder_ud)).grid(column=2, row=7)
+    ttk.Label(frame, text="Direction to Rotate").grid(column=1, row=8)
     ttk.Radiobutton(frame, text="Up", 
                     variable=shoulder_ud, 
                     value='U', 
-                    command=lambda *args: enable_btn(shoulder_btn, shoulder_lr, shoulder_ud)).grid(column=1, row=8)
+                    command=lambda *args: enable_btn2(shoulder_btn, deg_entry, shoulder_lr, shoulder_ud)).grid(column=1, row=9)
     ttk.Radiobutton(frame, text="Down", 
                     variable=shoulder_ud, 
                     value='D', 
-                    command=lambda *args: enable_btn(shoulder_btn, shoulder_lr, shoulder_ud)).grid(column=2, row=8)
-
+                    command=lambda *args: enable_btn2(shoulder_btn, deg_entry, shoulder_lr, shoulder_ud)).grid(column=2, row=9)
+    
     #auger handling UI
     spd_prof = StringVar()
     direction = StringVar()
     
     auger_btn = ttk.Button(frame, text="START AUGER", 
                               command=lambda *args: send_auger(spd_prof, direction), state='disabled')
-    auger_btn.grid(column=0, row=9)
-    ttk.Label(frame, text="Speed Proflie").grid(column=1, row=9)
-    ttk.Combobox(frame, textvariable=spd_prof, values=['OFF', 'LOW', 'HIGH']).grid(column=1, row=10)
-    ttk.Label(frame, text="Direction").grid(column=1, row=11)
+    auger_btn.grid(column=0, row=10)
+    ttk.Label(frame, text="Speed Profile").grid(column=1, row=10)
+    ttk.Combobox(frame, textvariable=spd_prof, values=['OFF', 'LOW', 'HIGH']).grid(column=1, row=11)
+    ttk.Label(frame, text="Direction").grid(column=1, row=12)
     ttk.Radiobutton(frame, text="CW", 
                     variable=direction, 
                     value='CW', 
-                    command=lambda *args: enable_btn(auger_btn, spd_prof, direction)).grid(column=1, row=12)
+                    command=lambda *args: enable_btn(auger_btn, spd_prof, direction)).grid(column=1, row=14)
     ttk.Radiobutton(frame, text="CCW", 
                     variable=direction, 
                     value='CCW', 
-                    command=lambda *args: enable_btn(auger_btn, spd_prof, direction)).grid(column=2, row=12)
+                    command=lambda *args: enable_btn(auger_btn, spd_prof, direction)).grid(column=2, row=14)
     
     #quit program
     ttk.Button(frame, text="QUIT", command=quit).grid(column=0, row=99)
